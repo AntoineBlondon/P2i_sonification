20e91285c12c4957bb200fdea69558914ae6cfb3
ablondon <antoine.blondon@insa-lyon.fr>
2025-05-07 16:51:06 +0200

    readded sonification button

--- interface.py
+++ interface.py
@@ -18,17 +18,20 @@ class Fenetre(tk.Tk):
         self.label_image.pack()
 
         # Bouton pour lancer la sonification
-        # self.bouton_sonifier = tk.Button(self, text=" ( Sonifier", command=self.sonifier_image, state=tk.DISABLED)
-        # self.bouton_sonifier.pack(pady=10)
+        self.bouton_sonifier = tk.Button(self, text="Sonifier", command=self.sonifier_image, state=tk.DISABLED)
+        self.bouton_sonifier.pack(pady=10)
 
     def charger_image(self):
         filepath = filedialog.askopenfilename(filetypes=[("Images", "*.png *.jpg *.jpeg *.bmp")])
         if filepath:
             self.image_pil = Image.open(filepath).convert("L")  # Convertir en niveaux de gris
-            image_redim = self.image_pil.resize((300, 300))
+            image_redim = self.image_pil.resize((300, 300)) # 
             self.photo_image = ImageTk.PhotoImage(image_redim)
             self.label_image.config(image=self.photo_image)
             self.bouton_sonifier.config(state=tk.NORMAL)
+    
+    def sonifier_image(self):
+        ...
 
 if __name__=="__main__":
 
