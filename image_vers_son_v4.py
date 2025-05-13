85b65bb1d5983a2abe60c0da77e58bcb78718543
ablondon <antoine.blondon@insa-lyon.fr>
2025-05-13 08:28:21 +0200

    worked on things

--- image_vers_son_v4.py
+++ image_vers_son_v4.py
@@ -3,14 +3,23 @@ from PIL import Image
 from mido import Message, MidiFile, MidiTrack
 import numpy
 from traitement import contour
-Dt = 50
+Dt = 500
 
-def add_chord_to_track(track, chord, time):
+def add_chord_to_track(track, chord):
     for note in chord:
         track.append(Message('note_on', note=note, velocity=64, time=0))
     for i, note in enumerate(chord):
-        track.append(Message('note_off', note=note, velocity=64, time=Dt))
+        first = chord[0]
+        track.append(Message('note_off', note=first, velocity=64, time=Dt))
 
+        for n in chord[1:]:
+            track.append(Message('note_off', note=n, velocity=64, time=0))
+
+
+def find_note(position):
+    min, max = 40, 70
+    #return int(20 + ((position+1) / 50) * 80) 
+    return int(min + ((position+1) / 50) * (max-min)) 
 
 
 def sonifier(image):
@@ -20,15 +29,14 @@ def sonifier(image):
     mid.tracks.append(track)
     track.append(Message('program_change', program=0, time=0))
     image = image.resize((50, 50))  # Taille raisonnable pour éviter un MIDI trop long
-    img = contour(numpy.asarray(image))
+    img = contour(numpy.asarray(image) > 50)
     time=0
     for y in numpy.transpose(img):
         chord = []
-        for i, x in enumerate(y):
+        for i, x in enumerate(reversed(y)):
             if x > 0:
-                note = int(20 + ((i+1) / 50) * 80) 
+                note =  find_note(i)
                 chord.append(note)
-        add_chord_to_track(track, chord, time)
-        time += Dt
+        add_chord_to_track(track, chord)
 
     mid.save("image_musique.mid")
